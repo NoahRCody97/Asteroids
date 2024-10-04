50ffa663ed9eb17ccc76b7f225815d1426c149ab
NoahCody <nrcody.97@gmail.com>
2024-10-04 14:07:25 -0700

    Update Main to create a black window + added .gitignore to ignore /venv and pycache

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
 
+def game_loop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+        pygame.Surface.fill(screen, color=(0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_loop()
